38a5cf7b6588cf59943df0d8dbe6570cd070419f
AleRojasGz <alejandro.rojasguzman18@gmail.com>
2022-11-10 18:50:49 -0300

    se agregan vista

--- src/components/Navbar_Cliente.js
+++ src/components/Navbar_Cliente.js
@@ -5,7 +5,7 @@ export const Navbar_Cliente = () => {
 
     return (
         <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: 280, height: '100vh' }}>
-            <Link to="/home">
+            <Link to="/clientes">
                 <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <svg className="bi pe-none me-2" width="40" height="32"></svg>
                     <span className="fs-4">Cliente</span>
@@ -22,7 +22,7 @@ export const Navbar_Cliente = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link to=''>
+                    <Link to='/hacerReserva'>
                         <a href="#" className="nav-link text-white">
                             <i className="fa-solid fa-home m-2" style={{ color: '#ffffff' }}></i>
                             Nueva Reserva
@@ -30,7 +30,7 @@ export const Navbar_Cliente = () => {
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to=''>
+                    <Link to='/misReservas'>
                         <a href="#" className="nav-link text-white" aria-current="page">
                             <i className="fa-regular fa-user m-2" style={{ color: '#ffffff' }}></i>
                             Listado Reservas
