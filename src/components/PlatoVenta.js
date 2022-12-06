2b8850e0af99c4f6ad62d1d32b36057cfbb10649
jorge <jorgemedelorte@gmail.com>
2022-12-06 20:38:13 -0300

    creacion de lista

--- src/components/PlatoVenta.js
+++ src/components/PlatoVenta.js
@@ -1,6 +1,39 @@
 import React from 'react'
 
 export const PlatoVenta = ({ image, title, description, price, buttonText, onPress, buttonEliminar, onPressE }) => {
+  
+  
+
+  var cart = {
+    items: [],
+    addItem: function(item) {
+      this.items.push(item);
+    },
+    removeItem: function(item) {
+      var index = this.items.indexOf(item);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+    },
+    getTotal: function() {
+      var total = 0;
+      for (var i = 0; i < this.items.length; i++) {
+        total += this.items[i].price;
+      }
+      return total;
+    },
+    getButtonText: function() {
+      if (this.items.length === 0) {
+        return 'Add to Cart';
+      } else {
+        return 'Remove from Cart';
+      }
+    }
+  };
+
+
+
+  
   return (
     <div className="col-sm-6 col-md-8">
     <div className="platoCard">
